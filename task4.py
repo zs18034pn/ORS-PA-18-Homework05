a6f3bb3a0afb2a5c25812b6974ddb266df46b912
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-18 14:28:20 +0100

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,15 @@
+"""
+===================   TASK 4   ====================
+* Name: Can String Be Float
+*
+* Write a script that will take integer number as
+* user input and return the product of digits. 
+* The script should be able to handle incorrect
+* input, as well as the negative integer numbers.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
