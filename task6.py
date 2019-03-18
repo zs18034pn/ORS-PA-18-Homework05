16985a2d0d75409d65696278d94215bd33dd9508
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-18 14:33:21 +0100

    task6

---
+++ task6.py
@@ -0,0 +1,18 @@
+"""
+===================   TASK 6   ====================
+* Name: Typewriter
+*
+* Write a script that will take file name as user
+* input. Then the script should take file content
+* as user input as well until user hits `Enter`.
+* At the end, the script should store the content
+* into the file with given name. If the file already
+* exists the script should append the new content
+* at the end.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
