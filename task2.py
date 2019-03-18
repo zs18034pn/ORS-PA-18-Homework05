76a020024957ac3cc2c27e5cb53ed02a4ac99ace
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-18 14:24:48 +0100

    task2.py

---
+++ task2.py
@@ -0,0 +1,14 @@
+"""
+===================   TASK 2   ====================
+* Name: Most Frequent Letter
+*
+* Write a script that takes the stirng as user
+* input and displays which letter has the most
+* occurences and how many. If two or more letters
+* have the same number of occurences print any.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+===================================================
+"""
