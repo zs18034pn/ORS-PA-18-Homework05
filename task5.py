35f3f61db518b6d92c3b7c94bb9c1b78ca641db7
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-18 14:30:06 +0100

    task5.py

---
+++ task5.py
@@ -0,0 +1,15 @@
+"""
+===================   TASK 5   ====================
+* Name: Average Value
+*
+* Write a function `averageval` that will take a
+* integer list as an argument and return the 
+* average value of the list elements.  
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work. It is not
+* allowed to use built-in functions.
+*
+* Use main() function to test your solution.
+===================================================
+"""
