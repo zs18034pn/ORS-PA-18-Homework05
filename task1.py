105beb1dd92fd8d107256fd49e94f3076820be02
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-18 14:22:39 +0100

    task1

---
+++ task1.py
@@ -0,0 +1,17 @@
+"""
+===================   TASK 1   ====================
+* Name: Power to the Number
+*
+* Write a function `numpower()` that will for the
+* passed based number `num` and exponent `expo`
+* return the value of the number `num` raised to
+* the power of `expo`.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work. It is not
+* allowed to use built-in operators and functions
+* for this task.
+*
+* Use main() function to test your solution.
+===================================================
+"""
