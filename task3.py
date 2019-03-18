1ed1691181105f2e203c3764fcecb6dd4fd20c52
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-18 14:26:45 +0100

    task3.py

---
+++ task3.py
@@ -0,0 +1,15 @@
+"""
+===================   TASK 3   ====================
+* Name: Convert to Octal
+*
+* Write a function `dec2oct` that will convert
+* positive integer number passed as argument into
+* the octal based number. 
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work. It is not
+* allowed to use built-in functions.
+*
+* Use main() function to test your solution.
+===================================================
+"""
